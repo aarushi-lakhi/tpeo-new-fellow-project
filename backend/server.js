c9b991dfd4dabe527547ac3cc0dee09a352af05c
ShivanshNikhra <shiv.nikhra@gmail.com>
2024-02-01 18:15:27 -0600

    Updated firebase.js and server.js with credentials

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,10 @@
 const express = require("express");
+require("dotenv").config();
+
 const app = express();
+
+const firebase = require("./firebase");
+const db = firebase.firestore;
 
 app.listen(4000, () => {
   console.log("Server running on port 4000");
