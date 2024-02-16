305adeaae0e63d3bed0f6e93541c6b6d65c4c856
ShivanshNikhra <shiv.nikhra@gmail.com>
2024-02-16 15:37:08 -0600

    Firebase Connected

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,12 +1,10 @@
-const admin = require("firebase-admin");
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY,
-  }),
-  databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
+  credential: admin.credential.cert(serviceAccount)
 });
 
-const db = admin.firestore();
+const db = admin.firestore(); 
+module.exports = db;  
--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,20 @@ require("dotenv").config();
 const app = express();
 
 const firebase = require("./firebase");
-const db = firebase.firestore;
+const db = require('./firebase')
+
+
+const tasksCollection = db.collection('Product');
+
+
+app.get("/tasks", async (req, res) => {
+  console.log("Endpoint was called"); 
+  const snapshot = await tasksCollection.get();
+  snapshot.forEach(doc => {
+    const data = doc.data(); 
+    console.log(data); 
+});
+})
 
 app.listen(4000, () => {
   console.log("Server running on port 4000");
