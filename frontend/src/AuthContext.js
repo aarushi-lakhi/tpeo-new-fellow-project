31a6c11f707a453220ff989dee043fedeee94b7a
Aarushi <aaru.lakhi@gmail.com>
2024-03-02 16:57:05 -0600

    resolved cors issue with popup for login and added navigation to home page

--- frontend/src/AuthContext.js
+++ frontend/src/AuthContext.js
@@ -1,4 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import {GoogleAuthProvider, signInWithPopup, signOut} from 'firebase/auth'; 
 import {auth} from './firebaseConfig'; 
 
@@ -9,13 +10,31 @@ export const useAuth = () => {
 }
 
 export const AuthProvider = ({ children }) => {
+    const navigate = useNavigate(); 
     const [currentUser, setCurrentUser] = useState(null);
     const [loading, setLoading] = useState(true);
   
-    const handleGoogleSignIn = async () => {
+    const handleGoogleSignIn = async (e) => {
       try {
-        const provider = new GoogleAuthProvider();
-        const result = await signInWithPopup(auth, provider);
+        const provider = await new GoogleAuthProvider();
+        signInWithPopup(auth, provider).then(async (result) => {
+          console.log(result);
+          const idToken = await result.user.getIdToken();
+          console.log(idToken); 
+
+          const userEmail = result.user.email;
+          const domainName = userEmail.split("@")[1]; 
+
+          if(domainName === "utexas.edu") {
+            console.log("Good Sign-In"); 
+            setCurrentUser(result.user);
+            navigate('/');
+          } else {
+            handleSignOut(); 
+            throw new Error("Invalid email domain. Please sign in with a @utexas.edu email."); 
+          }
+        })
+        /* const result = await signInWithPopup(auth, provider);
         const userEmail = result.user.email;
         const domainName = userEmail.split("@")[1]; 
   
@@ -24,7 +43,7 @@ export const AuthProvider = ({ children }) => {
         } else {
           await signOut(auth);
           throw new Error("Invalid email domain. Please sign in with a @utexas.edu email.");
-        }
+        } */
       } catch (error) {
         console.log("Error signing in with Google:", error);
         throw error;
