777ea1d749cf3761f0fcacb39fb2c61bccce8c04
Aarushi <aaru.lakhi@gmail.com>
2024-03-03 11:34:13 -0600

    finished image uploading process on pfp

--- backend/firebase.js
+++ backend/firebase.js
@@ -2,9 +2,11 @@ var admin = require("firebase-admin");
 
 var serviceAccount = require("./serviceAccountKey.json");
 
+//Initialize Firebase
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
+
 
 const db = admin.firestore(); 
 module.exports = db;  
--- frontend/src/Profile.js
+++ frontend/src/Profile.js
@@ -1,11 +1,57 @@
 import React from 'react';
+import {useState, useEffect} from 'react';
+import {storage} from './firebaseConfig';
+import {ref, uploadBytes, listAll, getDownloadURL} from 'firebase/storage';
+import {v4} from 'uuid';
 import { useNavigate } from 'react-router-dom';
 
 
 const Profile = () => {
+  const [imageUpload, setImageUpload] =  useState(null);
+  const [imageList, setImageList] = useState([]);
+  const imageListRef = ref(storage, "images/");
+
+  const uploadImage = () => {
+    if (imageUpload == null) {
+      return;
+    }
+
+    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+    uploadBytes(imageRef, imageUpload).then((snapshot) => {
+      //alert("Image Uploaded");
+      getDownloadURL(snapshot.ref).then((url) => {
+        setImageList((prev) => [...prev, url])
+      })
+    })
+  };
+
+  useEffect(() => {
+    listAll(imageListRef).then((response) => {
+      console.log(response);
+      response.items.forEach((item) => {
+        getDownloadURL(item).then((url) => {
+          setImageList((prev) => [...prev, url]);
+        })
+      })
+    })
+  }, [])
+  
   return (
     <div>
       <h1>Profile Page</h1>
+      <div className="fileUpload"> 
+        <input 
+          type="file" 
+          onChange={(event) => {
+            setImageUpload(event.target.files[0]);
+          }}
+        />
+        <button onClick={uploadImage}>Upload Image</button>
+
+        {imageList.map((url) => {
+          return <img src={url}/> 
+        })}
+      </div>
     </div>
   );
 };
