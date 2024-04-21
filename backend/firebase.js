a55a0d5b71385fdb8d9705fc072e99463dfd48a8
Aarushi <aaru.lakhi@gmail.com>
2024-04-20 19:18:14 -0500

    made env and modifed firebase.js

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,12 +1,9 @@
 var admin = require("firebase-admin");
-
-var serviceAccount = require("./serviceAccountKey.json");
-
+require('dotenv').config();
+var serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 //Initialize Firebase
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
-
-
-const db = admin.firestore(); 
-module.exports = db;  
+const db = admin.firestore();
+module.exports = db;
